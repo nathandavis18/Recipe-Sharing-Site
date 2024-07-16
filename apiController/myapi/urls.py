ec7836138221a88626b25b23a6a85634762cd11f
nathandavis18 <nathandavis8@mail.weber.edu>
2024-07-15 20:40:31 -0600

    Removed unused views

--- apiController/myapi/urls.py
+++ apiController/myapi/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
     path('recipe/', views.recipeList, name='recipe'),
     path('recipe/details/<int:id>', views.recipeDetails, name='details'),
     path('recipe/new', views.newRecipe, name='new')
